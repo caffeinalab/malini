09a776eda5f6dd981aa59893a2daf3620fa20733
Alberto Parziale <alberto.parziale@caffeina.com>
2020-04-02 15:16:25 +0200

    [~] update taxonomies, categories and tags decorator

--- Malini/Decorators/WithTaxonomies.php
+++ Malini/Decorators/WithTaxonomies.php
@@ -8,35 +8,44 @@ use Malini\Interfaces\PostDecoratorInterface;
 
 /**
  * The `taxonomies` decorator adds to the `Malini\Post` the list of taxonomies related to the current post.
- * 
+ *
  * Attributes added:
  * - `taxonomies`: an array containing this post taxonomies;
  * - `terms`: an array containing this post terms.
- * 
+ *
  * Options:
  * - `filter`: the attributes we want to retrieve
  */
-class WithTaxonomies extends PostDecorator implements PostDecoratorInterface {
-
-    public function decorate(Post $post, array $options = []) : Post {
+class WithTaxonomies extends PostDecorator implements PostDecoratorInterface
+{
+    public function decorate(Post $post, array $options = []): Post
+    {
         $wp_post = $post->wp_post;
+        $post_taxonomies = get_post_taxonomies($wp_post);
 
         $post->addRawAttributes([
-            'taxonomies'   => function() use ($wp_post) {
-                return get_post_taxonomies($wp_post);
+            'taxonomies' => function () use ($post_taxonomies) {
+                return $post_taxonomies;
             },
-            'terms' => function() use ($wp_post) {
-                return wp_get_post_terms($wp_post->ID);
-            }
+            'terms' => function () use ($wp_post, $post_taxonomies) {
+                $terms = [];
+                foreach ($post_taxonomies as $taxonomy) {
+                    $terms_by_taxonomy = get_the_terms($wp_post->ID, $taxonomy);
+                    if (is_array($terms_by_taxonomy)) {
+                        $terms = array_merge($terms, $terms_by_taxonomy);
+                    }
+                }
+
+                return $terms;
+            },
         ]);
 
         $this->filterConfig(
             $post,
             $options,
-            [ 'taxonomies', 'terms' ]
+            ['taxonomies', 'terms']
         );
 
         return $post;
     }
-
 }
